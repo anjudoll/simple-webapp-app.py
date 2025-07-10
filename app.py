432bceffafe2da664fa5364b0b0f44de4b3db9bd
anjudoll <sumayadavkanasani@gmail.com>
2025-07-10 11:57:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import flask
+app - Flask(_name_)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you")
+def hello():
+    return 'I am good, how about you?"
+
+  if _name_"" "_main_":
+      app.run()
+           
